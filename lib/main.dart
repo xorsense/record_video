ec5cd3662918d13557e2f67436a9c662125c6f33
Michael Christenson II <m3talsmith@gmail.com>
2023-11-04 17:29:06 -0400

    Working on iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,6 +34,8 @@ class _CameraScreenState extends State<CameraScreen> {
   bool _isLoading = true;
   late CameraController _cameraController;
 
+  bool _isRecording = false;
+
   _initCamera() async {
     final cameras = await availableCameras();
     final front = cameras.firstWhere((camera) => camera.lensDirection == CameraLensDirection.front);
@@ -39,6 +44,22 @@ class _CameraScreenState extends State<CameraScreen> {
     setState(() {
       _isLoading = false;
     });
+  }
+
+  _recordVideo() async {
+    if (_isRecording) {
+      final file = await _cameraController.stopVideoRecording();
+      setState(() => _isRecording = false);
+      final route = MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (_) => VideoPage(filePath: file.path),
+      );
+      Navigator.push(context, route);
+    } else {
+      await _cameraController.prepareForVideoRecording();
+      await _cameraController.startVideoRecording();
+      setState(() => _isRecording = true);
+    }
   }
 
   @override
@@ -63,6 +84,77 @@ class _CameraScreenState extends State<CameraScreen> {
         ),
       );
     }
-    return CameraPreview(_cameraController);
+    return Center(
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          CameraPreview(_cameraController),
+          Padding(
+            padding: const EdgeInsets.all(25),
+            child: FloatingActionButton(
+              backgroundColor: Colors.red,
+              child: Icon(_isRecording ? Icons.stop : Icons.circle),
+              onPressed: () => _recordVideo(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class VideoPage extends StatefulWidget {
+  final String filePath;
+
+  const VideoPage({Key? key, required this.filePath}) : super(key: key);
+
+  @override
+  _VideoPageState createState() => _VideoPageState();
+}
+
+class _VideoPageState extends State<VideoPage> {
+  late VideoPlayerController _videoPlayerController;
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  Future _initVideoPlayer() async {
+    _videoPlayerController = VideoPlayerController.file(File(widget.filePath));
+    await _videoPlayerController.initialize();
+    await _videoPlayerController.setLooping(true);
+    await _videoPlayerController.play();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Preview'),
+        elevation: 0,
+        backgroundColor: Colors.black26,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.check),
+            onPressed: () {
+              print('do something with the file');
+            },
+          )
+        ],
+      ),
+      extendBodyBehindAppBar: true,
+      body: FutureBuilder(
+        future: _initVideoPlayer(),
+        builder: (context, state) {
+          if (state.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return VideoPlayer(_videoPlayerController);
+          }
+        },
+      ),
+    );
   }
 }
